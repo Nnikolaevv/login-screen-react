1670d9bd7124ae872a9ca90ba21fd3208c882f04
Nikolay Shamov <nikolay.shamov@smi2.ru>
2021-11-11 21:08:08 +0300

    for gh

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
+import LoginForm from "./components/LoginForm";
 
 function App() {
-  const adminUser = {
-    email: 'admin@admin.com',
-    password: 'admin123'
-  }
+    const adminUser = {
+        email: 'admin@admin.com',
+        password: 'admin123'
+    }
 
-  return (
-    <div className="App">
+    const [user, setUser] = useState({name: '', email: ''})
+    const [error, setError] = useState('')
 
-    </div>
-  );
+    const Login = (details) => {
+        if (details.email === adminUser.email && details.password === adminUser.password) {
+            setUser({
+                name: details.name,
+                email: details.email
+            })
+            setError('')
+        } else {
+            setError(`Use ${adminUser.email} ${adminUser.password}`)
+        }
+    }
+
+    const Logout = () => {
+        setUser({name: '', email: ''});
+    }
+
+    return (
+        <div className="App">
+            {(user.email !== '')
+                ?
+                (<div className='welcome'>
+                    <h2>Welcome, <span>{user.name}</span></h2>
+                    <button onClick={Logout}>Logout</button>
+                </div>)
+                :
+                (<div>
+                    <LoginForm Login={Login} error={error}/>
+                </div>)
+            }
+        </div>
+    );
 }
 
 export default App;
